4f451d1aab9ce287891d11027961704c8395f820
karbafoo <karbafooo@gmail.com>
2021-09-08 22:22:35 +0430

    added servers

---
+++ app-1.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// parse application/json
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'daomart', 'build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'daomart', 'build', 'index.html'))
+);
+app.listen(80);
